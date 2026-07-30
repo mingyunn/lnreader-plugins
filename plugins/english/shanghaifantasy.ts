1b42fd5ce684c85584931dfef5963939af151852
mingyunn <kmmytn15@gmail.com>
2026-07-30 14:12:58 +0800

    shanghai fix

--- plugins/english/shanghaifantasy.ts
+++ plugins/english/shanghaifantasy.ts
@@ -42,7 +42,7 @@ class ShanghaiFantasy implements Plugin.PluginBase {
   name = 'Shanghai Fantasy';
   icon = 'src/en/shanghaifantasy/icon.png';
   site = 'https://shanghaifantasy.com';
-  version = '1.0.0';
+  version = '1.0.1';
   webStorageUtilized = false;
 
   filters = {
